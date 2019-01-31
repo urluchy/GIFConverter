ccd12a72ab3b94b2684dd2f5aac3548e3975a13d
urluchy <okolieursula@gmail.com>
2019-02-01 00:31:53 +0100

    Added Conversion, Error features, Font and Dependency

--- app/src/main/java/com/example/android/gifconverter/MainActivity.java
+++ app/src/main/java/com/example/android/gifconverter/MainActivity.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.cloudinary.Transformation;
 import com.cloudinary.android.MediaManager;
 import com.cloudinary.android.callback.ErrorInfo;
 import com.cloudinary.android.callback.UploadCallback;
@@ -84,10 +87,29 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onSuccess(String requestId, Map resultData) {
 
+                            Toast.makeText(MainActivity.this, "Uploaded successfully", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                            uploadButton.setVisibility(View.INVISIBLE);
+
+                            String publicId = resultData.get("public_id").toString();
+
+                            gifUrl = MediaManager.get().url().resourceType("video")
+                                    .transformation(new Transformation().videoSampling("25")
+                                    .delay("200").height("200").effect("loop:10").crop("scale"))
+                                    .format("gif").generate(publicId);
+
+                            Glide.with(getApplicationContext()).asGif().load(gifUrl).into(image1);
+                            downloadBtn.setVisibility(View.VISIBLE);
+
+
+
                         }
 
                         @Override
                         public void onError(String requestId, ErrorInfo error) {
+
+                            Toast.makeText(MainActivity.this, "Upload error", Toast.LENGTH_SHORT).show();
+                            Log.v("ERROR!!", error.getDescription());
 
                         }
 
